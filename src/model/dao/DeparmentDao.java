7b061d7d174fe7ece96bddc8ca58c780dd3ee0ff
Fabricio <fabriciopb90@gmail.com>
2026-06-04 13:07:19 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DeparmentDao.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package model.dao;
+
+import java.util.List;
+import model.entities.Department;
+
+/**
+ *
+ * @author User
+ */
+public interface DeparmentDao {
+    void insert(Department obj);
+    void upadte(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
